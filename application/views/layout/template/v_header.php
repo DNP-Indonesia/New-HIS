9c6784cff24429c1866adbc82c12bb6fd58620a2
Arief Rahman <koayrahmanr@gmail.com>
2023-06-23 12:49:08 +0700

    View Templates

---
+++ application/views/layout/template/v_header.php
@@ -0,0 +1,58 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<link href="dnp-logo.png" rel="icon">
+<title>DNP - HIS</title>
+
+<!-- Custom fonts for this template-->
+<link href="bootstrap/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+<!-- Custom styles for this template-->
+<link href="bootstrap/css/sb-admin-2.min.css" rel="stylesheet">
+
+<!-- Custom styles for this page -->
+<link href="bootstrap/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+<link href="bootstrap/datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
+
+<style type="text/css">
+    /* body{
+            padding-top:50px;
+            background-color:#34495e;
+        }
+
+        .hiddenRow {
+            padding: 0 !important;
+        } */
+
+    .modal-header {
+        background-color: #3498db;
+    }
+
+    .modal-title {
+        color: white;
+    }
+
+    .modal-content {
+        padding-top: 0px;
+        border-radius: 0;
+        border: 5px solid #3498db;
+    }
+
+    .hidden {
+        display: none;
+    }
+
+    .btn-purple {
+        background-color: #8000ff;
+        color: white;
+    }
+
+    .btn-purple:hover {
+        color: white;
+        background-color: #6906cc;
+    }
+</style>
---
+++ application/views/layout/template/v_test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'application/views/layout/template/v_header.php'; ?>
+</head>
+
+<body>
+    <?php include 'application/views/layout/template/v_body.php' ?>
+</body>
+</html>
