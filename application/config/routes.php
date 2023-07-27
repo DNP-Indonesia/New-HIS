b11d4ba261e716b8b5a5fcbd2da19107b971f66e
Muhammad Fadhil Maulana <95394393+ucupofcoffee@users.noreply.github.com>
2023-07-27 12:09:06 +0700

    Modify All

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,110 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'auth';
+$route['default_controller'] = 'index';
+$route['folder1/folder2/controller'] = 'folder1/folder2/ControllerName/method';
+$route['Sundries/Barang'] = 'Sundries/Barang/c_barang/barangpage';
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+// c_atuh
+$route['auth'] = 'auth/c_auth';
+$route['home'] = 'auth/c_auth/home';
+$route['logout'] = 'auth/c_auth/logout';
+$route['dashboard'] = 'Page_his/home';
+$route['data-user'] = 'Page_his/user';
+
+// Personal Data
+$route['data-karyawan'] = 'Page_his/karyawan';
+$route['data-karyawan-keluar'] = 'Page_his/karyawan_out';
+$route['data-karyawan-training-dan-percobaan'] = 'Page_his/karyawan_temp';
+$route['data-karyawan-training-dan-percobaan-keluar'] = 'Page_his/karyawan_out_temp';
+$route['daftar-divisi'] = 'Page_his/divisi';
+$route['daftar-departemen'] = 'Page_his/departemen';
+$route['daftar-section'] = 'Page_his/section';
+$route['daftar-shift'] = 'Page_his/shift';
+$route['daftar-jabatan'] = 'Page_his/jabatan';
+$route['daftar-golongan'] = 'Page_his/golongan';
+
+
+# -------------------------- SUNDRIES -------------------------- #
+// User
+
+
+// Barang
+$route['barang'] = 'Sundries/Barang/c_barang/index';
+$route['addbarang'] = 'Sundries/Barang/c_barang/addBarang';
+$route['addbarangother'] = 'Sundries/Barang/c_barang/addBarangOther';
+$route['updatebarang'] = 'Sundries/Barang/c_barang/updateBarang';
+$route['deletebarang/(:any)'] = 'Sundries/Barang/c_barang/deleteBarang/$1';
+
+// Jenis
+$route['jenis'] = 'Sundries/Master/c_jenis/index';
+$route['addjenis'] = 'Sundries/Master/c_jenis/addJenis';
+$route['updatejenis'] = 'Sundries/Master/c_jenis/updateJenis';
+$route['deletejenis/(:any)'] = 'Sundries/Master/c_jenis/deleteJenis/$1';
+
+// Kategori
+$route['kategori'] = 'Sundries/Master/c_kategori/index';
+$route['addkategori'] = 'Sundries/Master/c_kategori/addKategori';
+$route['updatekategori'] = 'Sundries/Master/c_kategori/update';
+$route['deletekategori/(:any)'] = 'Sundries/Master/c_kategori/deleteKategori/$1';
+
+// Estimasi
+$route['estimasi'] = 'Sundries/Transaksi/c_estimasi/index';
+$route['addkeranjangestimasi'] = 'Sundries/Transaksi/c_estimasi/addKeranjang';
+$route['showkeranjangestimasi'] = 'Sundries/Transaksi/c_estimasi/showKeranjang';
+$route['deletekeranjangestimasi'] = 'Sundries/Transaksi/c_estimasi/deleteKeranjang';
+$route['addestimasi'] = 'Sundries/Transaksi/c_estimasi/addEstimasi';
+$route['deleteestimasi'] = 'Sundries/Transaksi/c_estimasi/deleteEstimasi';
+$route['detailestimasi'] = 'Sundries/Transaksi/c_estimasi/detailEstimasi';
+$route['printestimasi'] = 'Sundries/Transaksi/c_estimasi/printEstimasi';
+$route['approveestimasi'] = 'Sundries/Transaksi/c_estimasi/approveEstimasi';
+$route['rejectestimasi'] = 'Sundries/Transaksi/c_estimasi/rejectEstimasi';
+
+// Konsumsi
+$route['konsumsi'] = 'Sundries/Transaksi/c_konsumsi/index';
+$route['barangfaktur'] = 'Sundries/Transaksi/c_konsumsi/barangFaktur';
+$route['addkeranjangkonsumsi'] = 'Sundries/Transaksi/c_konsumsi/addKeranjang';
+$route['showkeranjangkonsumsi'] = 'Sundries/Transaksi/c_konsumsi/showKeranjang';
+$route['deletekeranjangkonsumsi'] = 'Sundries/Transaksi/c_konsumsi/deleteKeranjang';
+$route['addkonsumsi'] = 'Sundries/Transaksi/c_konsumsi/addKonsumsi';
+$route['deletekonsumsi'] = 'Sundries/Transaksi/c_konsumsi/deleteKonsumsi';
+$route['detailkonsumsi'] = 'Sundries/Transaksi/c_konsumsi/detailKonsumsi';
+$route['printkonsumsi'] = 'Sundries/Transaksi/c_konsumsi/printKonsumsi';
+$route['approvekonsumsi'] = 'Sundries/Transaksi/c_konsumsi/approveKonsumsi';
+$route['rejectkonsumsi'] = 'Sundries/Transaksi/c_konsumsi/rejectKonsumsi';
+
+// Pembelian
+$route['pembelian'] = 'Sundries/Transaksi/c_pembelian/index';
+$route['addkeranjangpembelian'] = 'Sundries/Transaksi/c_pembelian/addKeranjang';
+$route['showkeranjangpembelian'] = 'Sundries/Transaksi/c_pembelian/showKeranjang';
+$route['deletekeranjangpembelian'] = 'Sundries/Transaksi/c_pembelian/deleteKeranjang';
+$route['addpembelian'] = 'Sundries/Transaksi/c_pembelian/addPembelian';
+$route['detailpembelian'] = 'Sundries/Transaksi/c_pembelian/detailPembelian';
+$route['printpembelian'] = 'Sundries/Transaksi/c_pembelian/printPembelian';
+$route['formpembelian'] = 'Sundries/Transaksi/c_pembelian/formPembelian';
+
+// Penerimaan
+$route['penerimaan'] = 'Sundries/Transaksi/c_penerimaan/index';
+$route['addpenerimaan'] = 'Sundries/Transaksi/c_penerimaan/addPenerimaan';
+$route['formpenerimaan'] = 'Sundries/Transaksi/c_penerimaan/formPenerimaan';
+
+// Permintaan
+$route['permintaan'] = 'Sundries/Transaksi/c_permintaan/index';
+$route['cekkeranjangpermintaan'] = 'Sundries/Transaksi/c_permintaan/cekKeranjang';
+$route['showkeranjangpermintaan'] = 'Sundries/Transaksi/c_permintaan/showKeranjang';
+$route['deletekeranjangpermintaan'] = 'Sundries/Transaksi/c_permintaan/deleteKeranjang';
+$route['addpermintaan'] = 'Sundries/Transaksi/c_permintaan/addPermintaan';
+$route['deletepermintaan'] = 'Sundries/Transaksi/c_permintaan/deletePermintaan';
+$route['printpermintaan'] = 'Sundries/Transaksi/c_permintaan/printPermintaan';
+$route['approvepermintaan'] = 'Sundries/Transaksi/c_permintaan/approvePermintaan';
+$route['rejectpermintaan'] = 'Sundries/Transaksi/c_permintaan/rejectPermintaan';
+$route['addbarang'] = 'Sundries/Transaksi/c_permintaan/addBarang';
+$route['deletebarang'] = 'Sundries/Transaksi/c_permintaan/deleteBarang';
+$route['detailbarang'] = 'Sundries/Transaksi/c_permintaan/detailBarang';
+$route['permintaanulang'] = 'Sundries/Transaksi/c_permintaan/permintaanUlang';
+$route['permintaanproses'] = 'Sundries/Transaksi/c_permintaan/permintaanProses';
+$route['permintaanselesai'] = 'Sundries/Transaksi/c_permintaan/permintaanSelesai';
+$route['permintaansiap'] = 'Sundries/Transaksi/c_permintaan/permintaanSiap';
--- application/controllers/Page_his.php
+++ application/controllers/Page_his.php
@@ -1,122 +1,132 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-class Page_his extends MY_Controller {
-  public function __construct(){
+defined('BASEPATH') or exit('No direct script access allowed');
+class Page_his extends MY_Controller
+{
+  public function __construct()
+  {
     parent::__construct();
-    $this->load->model('M_his');
+    $this->load->model('Master/m_karyawan');
   }
 
-  public function home(){
+  public function home()
+  {
     $menu = 'hom';
     $data[''] = "";
-  
+
     // $data['sidebar'] = $this->load->view('side_medical/admin_medical');
-    if ($this->session->userdata('role')=='sdr_Admin Bagian'
-      OR $this->session->userdata('role')=='sdr_Kepala Bagian'
-      OR $this->session->userdata('role')=='sdr_Admin Gudang'
-      OR $this->session->userdata('role')=='sdr_Kepala Gudang'){
-      redirect('Sundries/requestsundriescontroller/dashboard');
-    }else{
-      $this->render_backend('v_dashboard', $menu, $data); 
+    if (
+      $this->session->userdata('role') == 'sdr_Admin Bagian'
+      or $this->session->userdata('role') == 'sdr_Kepala Bagian'
+      or $this->session->userdata('role') == 'sdr_Admin Gudang'
+      or $this->session->userdata('role') == 'sdr_Kepala Gudang'
+    ) {
+      redirect('Sundries/Transaksi/c_permintaan/dashboard');
+    } else {
+      $this->render_backend('layout/v_dashboard', $menu, $data);
     }
   }
 
-
-  public function user(){
-    $data['user'] = $this->M_his->data_user();
+  // tampil role super user
+  public function user()
+  {
+    $data['user'] = $this->m_karyawan->data_user();
 
     $menu = 'user';
 
-    if($this->session->userdata('role') == 'super_user'){
+    if ($this->session->userdata('role') == 'super_user') {
       $this->render_backend('v_user', $menu, $data);
-    }else{
-      $this->render_backend('v_error', $menu, $data);
-    }
-  }
-
-  public function karyawan(){
-    $menu = $this->uri->segment(2);
-    
-    $data['karyawan'] = $this->M_his->data_karyawan();
-
-    if($this->session->userdata('role') == 'super_user'){
-        $this->render_backend('v-his_karyawan', $menu, $data);     
     } else {
       $this->render_backend('v_error', $menu, $data);
     }
   }
 
-  public function karyawan_out(){
+  // 
+  public function karyawan()
+  {
     $menu = $this->uri->segment(2);
-    
-    $data['karyawan_out'] = $this->M_his->data_karyawan_out();
 
-    $this->render_backend('v-his_karyawan_out', $menu, $data);
+    $data['karyawan'] = $this->m_karyawan->data_karyawan();
+
+    if ($this->session->userdata('role') == 'super_user') {
+      $this->render_backend('Sundries/Personal/v_karyawan.php', $menu, $data);
+    } else {
+      $this->render_backend('v_error', $menu, $data);
+    }
   }
 
-  public function karyawan_temp(){
+  public function karyawan_out()
+  {
     $menu = $this->uri->segment(2);
-    
-    $data['karyawan'] = $this->M_his->data_karyawan_temp();
 
-    $this->render_backend('v-his_karyawan_temp', $menu, $data);
+    $data['karyawan_out'] = $this->m_karyawan->data_karyawan_out();
+    
+    $this->render_backend('Sundries/Personal/v_karyawan_keluar.php', $menu, $data);
   }
 
-  public function karyawan_out_temp(){
+  public function karyawan_temp()
+  {
     $menu = $this->uri->segment(2);
-    
-    $data['karyawan'] = $this->M_his->data_karyawan_out_temp();
 
-    $this->render_backend('v-his_karyawan_out_temp', $menu, $data);
+    $data['karyawan'] = $this->m_karyawan->data_karyawan_temp();
+
+    $this->render_backend('Sundries/Personal/v_pelatihan_coba.php', $menu, $data);
   }
 
-  function divisi(){
+  public function karyawan_out_temp()
+  {
     $menu = $this->uri->segment(2);
-    $data['div'] = $this->M_his->data_divisi();
 
-    $this->render_backend('v-his_divisi', $menu, $data);
-    
+    $data['karyawan'] = $this->m_karyawan->data_karyawan_out_temp();
+
+    $this->render_backend('Sundries/Personal/v_pelatihan_keluar', $menu, $data);
   }
 
-  function departemen(){
+  function divisi()
+  {
     $menu = $this->uri->segment(2);
-    $data['dep'] = $this->M_his->data_dep();
-    // $data['section'] = $this->M_his->data_section_byId();
+    $data['div'] = $this->m_karyawan->data_divisi();
 
-    $this->render_backend('v-his_departemen', $menu, $data);
-    
+    $this->render_backend('Sundries/Personal/v_divisi', $menu, $data);
   }
 
-  function section(){
+  function departemen()
+  {
     $menu = $this->uri->segment(2);
-    $data['section'] = $this->M_his->data_section();
+    $data['dep'] = $this->m_karyawan->data_dep();
+    // $data['section'] = $this->Master/m_karyawan->data_section_byId();
 
-    $this->render_backend('v-his_section', $menu, $data);
-    
+    $this->render_backend('Sundries/Personal/v_departemen', $menu, $data);
   }
 
-  function shift(){
+  function section()
+  {
     $menu = $this->uri->segment(2);
-    $data['shift'] = $this->M_his->data_shift();
+    $data['section'] = $this->m_karyawan->data_section();
 
-    $this->render_backend('v-his_shift', $menu, $data);
-    
+    $this->render_backend('Sundries/Personal/v_section', $menu, $data);
   }
 
-  function jabatan(){
+  function shift()
+  {
     $menu = $this->uri->segment(2);
-    $data['jabatan'] = $this->M_his->data_jabatan();
+    $data['shift'] = $this->m_karyawan->data_shift();
 
-    $this->render_backend('v-his_jabatan', $menu, $data);
-    
+    $this->render_backend('Sundries/Personal/v_shift', $menu, $data);
   }
 
-  function golongan(){
+  function jabatan()
+  {
     $menu = $this->uri->segment(2);
-    $data['golongan'] = $this->M_his->data_golongan();
+    $data['jabatan'] = $this->m_karyawan->data_jabatan();
 
-    $this->render_backend('v-his_golongan', $menu, $data);
-    
+    $this->render_backend('Sundries/Personal/v_jabatan', $menu, $data);
   }
 
+  function golongan()
+  {
+    $menu = $this->uri->segment(2);
+    $data['golongan'] = $this->m_karyawan->data_golongan();
+
+    $this->render_backend('Sundries/Personal/v_golongan', $menu, $data);
+  }
 }
