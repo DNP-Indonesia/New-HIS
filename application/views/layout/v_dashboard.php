703739d63f6764c6af4139ade1cf0e552919cc3b
Arief Rahman <koayrahmanr@gmail.com>
2023-09-01 08:04:11 +0700

    clean code

--- application/views/layout/v_dashboard.php
+++ application/views/layout/v_dashboard.php
@@ -1,75 +1,73 @@
-
-    <div class="container-fluid">
-        <!-- 404 Error Text -->
-        <div class="text-center">
-            <h1>
-                <div data-text="DNP">DNP</div>
-            </h1>
-            <p class="lead text-gray-800 mb-5">HRCA Information System</p>
-            <p class="text-gray-500 mb-0">Selamat datang, Salam Sehat!</p>
-        </div>
+<div class="container-fluid">
+    <!-- 404 Error Text -->
+    <div class="text-center">
+        <h1>
+            <div data-text="DNP">DNP</div>
+        </h1>
+        <p class="lead text-gray-800 mb-5">HRCA Information System</p>
+        <p class="text-gray-500 mb-0">Selamat datang, Salam Sehat!</p>
     </div>
-    <br>
-    <!-- Modul yang tersedia di HIS -->
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-4">
-                <a href="<?= base_url('index.php/page_his/home_personalia') ?>" style="text-decoration: none;">
-                    <div class="card text-white mb-3">
-                        <div class="card-body">
-                            <h5 class="card-title">Personalia</h5>
-                        </div>
+</div>
+<br>
+<!-- Modul yang tersedia di HIS -->
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <a href="<?= base_url('index.php/page_his/home_personalia') ?>" style="text-decoration: none;">
+                <div class="card text-white mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">Personalia</h5>
                     </div>
-                </a>
-            </div>
-            <!-- Tambahkan button-menu lain di sini -->
-            <div class="col-sm-4">
-                <a href="<?= base_url('index.php/page_his/karyawan_out') ?>" style="text-decoration: none;">
-                    <div class="card text-white  mb-3">
-                        <div class="card-body">
-                            <h5 class="card-title">DIV LAIN</h5>
-                            
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-sm-4">
-                <a href="<?= base_url('index.php/page_his/karyawan_temp') ?>" style="text-decoration: none;">
-                    <div class="card text-white  mb-3">
-                        <div class="card-body">
-                            <h5 class="card-title">DIV LAIN</h5>
-                           
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-sm-4">
-                <a href="<?= base_url('index.php/page_his/karyawan_temp') ?>" style="text-decoration: none;">
-                    <div class="card text-white mb-3">
-                        <div class="card-body">
-                            <h5 class="card-title">DIV LAIN</h5>
-                           
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-sm-4">
-                <a href="<?= base_url('index.php/page_his/karyawan_temp') ?>" style="text-decoration: none;">
-                    <div class="card text-white mb-3">
-                        <div class="card-body">
-                            <h5 class="card-title">DIV LAIN</h5>
-                           
-                        </div>
-                    </div>
-                </a>
-            </div>
-            
+                </div>
+            </a>
         </div>
+        <!-- Tambahkan button-menu lain di sini -->
+        <div class="col-sm-4">
+            <a href="<?= base_url('index.php/page_his/karyawan_out') ?>" style="text-decoration: none;">
+                <div class="card text-white  mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">DIV LAIN</h5>
+
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-sm-4">
+            <a href="<?= base_url('index.php/page_his/karyawan_temp') ?>" style="text-decoration: none;">
+                <div class="card text-white  mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">DIV LAIN</h5>
+
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-sm-4">
+            <a href="<?= base_url('index.php/page_his/karyawan_temp') ?>" style="text-decoration: none;">
+                <div class="card text-white mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">DIV LAIN</h5>
+
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-sm-4">
+            <a href="<?= base_url('index.php/page_his/karyawan_temp') ?>" style="text-decoration: none;">
+                <div class="card text-white mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">DIV LAIN</h5>
+
+                    </div>
+                </div>
+            </a>
+        </div>
+
     </div>
+</div>
 
-    <style scoped>
-
+<style scoped>
     .card {
         position: relative;
         transition: transform 0.3s, box-shadow 0.3s;
@@ -77,10 +75,12 @@
         background: #1cc88a;
     }
 
-    .card:hover, .card:focus {
+    .card:hover,
+    .card:focus {
         transform: translateY(-5px);
         box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
-        background-color: #1cc88a; /* Warna hijau saat tombol melayang */
+        background-color: #1cc88a;
+        /* Warna hijau saat tombol melayang */
     }
 
     .card:active {
@@ -91,6 +91,4 @@
     .card-body {
         padding: 1.25rem;
     }
-
 </style>
-
