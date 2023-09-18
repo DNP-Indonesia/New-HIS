ea2bf887ce7c2915271b76b9fdb7cdb03677521a
mfadhillll <95394393+ucupofcoffee@users.noreply.github.com>
2023-09-18 12:43:35 +0700

    Update m_estimasi.php

--- application/models/Sundries/Transaksi/m_estimasi.php
+++ application/models/Sundries/Transaksi/m_estimasi.php
@@ -52,6 +52,19 @@ class m_estimasi extends CI_Model
             ->result();
     }
 
+    public function getSetuju()
+    {
+        return $this->db->from($this->table)
+            ->join('tbl_user', 'tbl_user.id_user=' . $this->table . '.id_user')
+            ->join('his_section', 'his_section.id_section=tbl_user.id_section')
+            ->join($this->tabletolak, $this->tabletolak . '.faktur=' . $this->table . '.faktur')
+            ->where($this->table . '.id_user', $this->session->userdata('id_user'))
+            ->where('status', 'Disetujui')
+            ->order_by($this->primaryKey, 'DESC')
+            ->group_by($this->tabletolak . '.faktur')
+            ->get()
+            ->result();
+    }
     public function getTolak()
     {
         return $this->db->from($this->table)
