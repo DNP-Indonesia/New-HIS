f31758de34d4a8b4fceddfe9bbb8928e16ff0b4e
mfadhillll <95394393+ucupofcoffee@users.noreply.github.com>
2023-09-19 10:29:06 +0700

    Revert "recycle"
    This reverts commit 6ffd4731d822a43b1a8e3c15771b6be32100bb80, reversing
    changes made to 21071b534e4b5a4e1711a0a8365d8d3544e3595c.

---
+++ application/config/routes.php
@@ -0,0 +1,171 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------------
+| URI ROUTING
+| -------------------------------------------------------------------------
+| This file lets you re-map URI requests to specific controller functions.
+|
+| Typically there is a one-to-one relationship between a URL string
+| and its corresponding controller class/method. The segments in a
+| URL normally follow this pattern:
+|
+|	example.com/class/method/id/
+|
+| In some instances, however, you may want to remap this relationship
+| so that a different class/function is called than the one
+| corresponding to the URL.
+|
+| Please see the user guide for complete details:
+|
+|	https://codeigniter.com/user_guide/general/routing.html
+|
+| -------------------------------------------------------------------------
+| RESERVED ROUTES
+| -------------------------------------------------------------------------
+|
+| There are three reserved routes:
+|
+|	$route['default_controller'] = 'welcome';
+|
+| This route indicates which controller class should be loaded if the
+| URI contains no data. In the above example, the "welcome" class
+| would be loaded.
+|
+|	$route['404_override'] = 'errors/page_missing';
+|
+| This route will tell the Router which controller/method to use if those
+| provided in the URL cannot be matched to a valid route.
+|
+|	$route['translate_uri_dashes'] = FALSE;
+|
+| This is not exactly a route, but allows you to automatically route
+| controller and method names that contain dashes. '-' isn't a valid
+| class or method name character, so it requires translation.
+| When you set this option to TRUE, it will replace ALL dashes in the
+| controller and method URI segments.
+|
+| Examples:	my-controller/index	-> my_controller/index
+|		my-controller/my-method	-> my_controller/my_method
+*/
+$route['default_controller'] = 'index';
+$route['folder1/folder2/controller'] = 'folder1/folder2/ControllerName/method';
+$route['Sundries/Barang'] = 'Sundries/Barang/c_barang/index';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
+
+// c_atuh
+$route['auth'] = 'auth/c_auth';
+$route['home'] = 'auth/c_auth/home';
+$route['logout'] = 'auth/c_auth/logout';
+$route['dashboard'] = 'Master/Page_his/home';
+
+$route['data-user'] = 'Auth/c_role/user';
+
+// Personal Data
+$route['data-karyawan'] = 'Master/Page_his/karyawan';
+$route['data-karyawan-keluar'] = 'Master/Page_his/karyawan_out';
+$route['data-karyawan-training-dan-percobaan'] = 'Master/Page_his/karyawan_temp';
+$route['data-karyawan-training-dan-percobaan-keluar'] = 'Master/Page_his/karyawan_out_temp';
+$route['daftar-divisi'] = 'Master/Page_his/divisi';
+$route['daftar-departemen'] = 'Master/Page_his/departemen';
+$route['daftar-section'] = 'Master/Page_his/section';
+$route['daftar-shift'] = 'Master/Page_his/shift';
+$route['daftar-jabatan'] = 'Master/Page_his/jabatan';
+$route['daftar-golongan'] = 'Master/Page_his/golongan';
+
+// Personal Data
+$route['data-karyawan'] = 'Master/Page_his/karyawan';
+$route['data-karyawan-keluar'] = 'Master/Page_his/karyawan_out';
+$route['data-karyawan-training-dan-percobaan'] = 'Master/Page_his/karyawan_temp';
+$route['data-karyawan-training-dan-percobaan-keluar'] = 'Master/Page_his/karyawan_out_temp';
+$route['daftar-divisi'] = 'Master/Page_his/divisi';
+$route['daftar-departemen'] = 'Master/Page_his/departemen';
+$route['daftar-section'] = 'Master/Page_his/section';
+$route['daftar-shift'] = 'Master/Page_his/shift';
+$route['daftar-jabatan'] = 'Master/Page_his/jabatan';
+$route['daftar-golongan'] = 'Master/Page_his/golongan';
+
+
+// Barang
+$route['barang'] = 'Sundries/Barang/c_barang/index';
+$route['addbarang'] = 'Sundries/Barang/c_barang/addBarang';
+$route['addbarangother'] = 'Sundries/Barang/c_barang/addBarangOther';
+$route['updatebarang/(:any)'] = 'Sundries/Barang/c_barang/updateBarang/$1';
+$route['deletebarang/(:any)'] = 'Sundries/Barang/c_barang/deleteBarang/$1';
+
+// Jenis
+$route['jenis'] = 'Sundries/Master/c_jenis/index';
+$route['addjenis'] = 'Sundries/Master/c_jenis/addJenis';
+$route['updatejenis/(:any)'] = 'Sundries/Master/c_jenis/updateJenis/$1';
+$route['deletejenis/(:any)'] = 'Sundries/Master/c_jenis/deleteJenis/$1';
+
+// Kategori
+$route['kategori'] = 'Sundries/Master/c_kategori/index';
+$route['addkategori'] = 'Sundries/Master/c_kategori/addKategori';
+$route['updatekategori/(:any)'] = 'Sundries/Master/c_kategori/update/$1';
+$route['deletekategori/(:any)'] = 'Sundries/Master/c_kategori/deleteKategori/$1';
+
+// Estimasi
+$route['estimasi'] = 'Sundries/Transaksi/c_estimasi/index';
+$route['addkeranjangestimasi'] = 'Sundries/Transaksi/c_estimasi/addKeranjang';
+$route['showkeranjangestimasi'] = 'Sundries/Transaksi/c_estimasi/showKeranjang';
+$route['deletekeranjangestimasi'] = 'Sundries/Transaksi/c_estimasi/deleteKeranjang';
+$route['addestimasi'] = 'Sundries/Transaksi/c_estimasi/addEstimasi';
+$route['deleteestimasi/(:any)'] = 'Sundries/Transaksi/c_estimasi/deleteEstimasi/$1';
+$route['detailestimasi/(:any)'] = 'Sundries/Transaksi/c_estimasi/detailEstimasi/$1';
+$route['printestimasi'] = 'Sundries/Transaksi/c_estimasi/printEstimasi';
+$route['approveestimasi'] = 'Sundries/Transaksi/c_estimasi/approveEstimasi';
+$route['rejectestimasi'] = 'Sundries/Transaksi/c_estimasi/rejectEstimasi';
+
+// Konsumsi
+$route['konsumsi'] = 'Sundries/Transaksi/c_konsumsi/index';
+$route['barangfaktur'] = 'Sundries/Transaksi/c_konsumsi/barangFaktur';
+$route['addkeranjangkonsumsi'] = 'Sundries/Transaksi/c_konsumsi/addKeranjang';
+$route['showkeranjangkonsumsi'] = 'Sundries/Transaksi/c_konsumsi/showKeranjang';
+$route['deletekeranjangkonsumsi'] = 'Sundries/Transaksi/c_konsumsi/deleteKeranjang';
+$route['addkonsumsi'] = 'Sundries/Transaksi/c_konsumsi/addKonsumsi';
+$route['deletekonsumsi/(:any)'] = 'Sundries/Transaksi/c_konsumsi/deleteKonsumsi/$1';
+$route['detailkonsumsi/(:any)'] = 'Sundries/Transaksi/c_konsumsi/detailKonsumsi/$1';
+$route['printkonsumsi'] = 'Sundries/Transaksi/c_konsumsi/printKonsumsi';
+$route['approvekonsumsi'] = 'Sundries/Transaksi/c_konsumsi/approveKonsumsi';
+$route['rejectkonsumsi'] = 'Sundries/Transaksi/c_konsumsi/rejectKonsumsi';
+
+// Pembelian
+$route['pembelian'] = 'Sundries/Transaksi/c_pembelian/index';
+$route['formpembelian'] = 'Sundries/Transaksi/c_pembelian/formPembelian';
+$route['addkeranjangpembelian'] = 'Sundries/Transaksi/c_pembelian/addKeranjang';
+$route['showkeranjangpembelian'] = 'Sundries/Transaksi/c_pembelian/showKeranjang';
+$route['deletekeranjangpembelian'] = 'Sundries/Transaksi/c_pembelian/deleteKeranjang';
+$route['addpembelian'] = 'Sundries/Transaksi/c_pembelian/addPembelian';
+$route['detailpembelian/(:any)'] = 'Sundries/Transaksi/c_pembelian/detailPembelian/$1';
+$route['printpembelian'] = 'Sundries/Transaksi/c_pembelian/printPembelian';
+$route['formpembelian'] = 'Sundries/Transaksi/c_pembelian/formPembelian';
+
+// Penerimaan
+$route['penerimaan'] = 'Sundries/Transaksi/c_penerimaan/index';
+$route['addpenerimaan'] = 'Sundries/Transaksi/c_penerimaan/addPenerimaan';
+$route['formpenerimaan'] = 'Sundries/Transaksi/c_penerimaan/formPenerimaan';
+
+// Permintaan
+$route['permintaan'] = 'Sundries/Transaksi/c_permintaan/index';
+$route['detailpermintaan/(:any)'] = 'Sundries/Transaksi/c_permintaan/detail/$1';
+$route['updatepermintaan/(:any)'] = 'Sundries/Transaksi/c_permintaan/updateJumlah/$1';
+$route['cekkeranjangpermintaan'] = 'Sundries/Transaksi/c_permintaan/cekKeranjang';
+$route['showkeranjangpermintaan'] = 'Sundries/Transaksi/c_permintaan/showKeranjang';
+$route['deletekeranjangpermintaan'] = 'Sundries/Transaksi/c_permintaan/deleteKeranjang';
+$route['addpermintaan'] = 'Sundries/Transaksi/c_permintaan/addPermintaan';
+$route['deletepermintaan/(:any)'] = 'Sundries/Transaksi/c_permintaan/deletePermintaan/$1';
+$route['printpermintaan/(:any)'] = 'Sundries/Transaksi/c_permintaan/printPermintaan/';
+$route['approvepermintaan/(:any)'] = 'Sundries/Transaksi/c_permintaan/approvePermintaan/$1';
+$route['rejectpermintaan'] = 'Sundries/Transaksi/c_permintaan/rejectPermintaan';
+$route['addbarang'] = 'Sundries/Transaksi/c_permintaan/addBarang';
+$route['deletebarangpermintaan/(:any)'] = 'Sundries/Transaksi/c_permintaan/deleteBarang/$1';
+$route['detailbarang'] = 'Sundries/Transaksi/c_permintaan/detailBarang';
+$route['permintaanulang/(:any)'] = 'Sundries/Transaksi/c_permintaan/permintaanUlang/$1';
+$route['permintaanproses/(:any)'] = 'Sundries/Transaksi/c_permintaan/permintaanProses/$1';
+$route['permintaanselesai/(:any)'] = 'Sundries/Transaksi/c_permintaan/permintaanSelesai/$1';
+$route['permintaansiap/(:any)'] = 'Sundries/Transaksi/c_permintaan/permintaanSiap/$1';
+
+
---
+++ application/views/personalData/dashboard.php
@@ -0,0 +1,127 @@
+<div class="container-fluid">
+        <!-- 404 Error Text -->
+        <div class="text-center">
+            <h1>
+                <div data-text="DNP">Personalia</div>
+            </h1>
+            <p class="lead text-gray-800 mb-5">HRCA Information System</p>
+            
+        </div>
+        
+    </div>
+    <a href="<?php echo base_url('dashboard')?>" class="back-link" >
+			<i class="fas fa-arrow-left fa-sm text-white-50"></i> Kembali
+		</a>
+
+        <br>
+        <br>
+
+    <div class="card shadow mb-2">
+            <div class="card-body">
+                <div class="table-responsive">
+                <table class="table" id='dataTable' >
+                    <thead>
+                        <tr>
+                            <th>Menu</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><a href="<?= base_url('Master/Page_his/karyawan') ?>" class="menu-link">Daftar Karyawan</a></td>
+                        </tr>
+                        <tr>
+                            
+                            <td><a href="<?= base_url('Master/Page_his/karyawan_out') ?>" class="menu-link">Daftar Karyawan Keluar</a></td>
+                        </tr>
+                        <tr>
+                            
+                            <td><a href="<?= base_url('Master/Page_his/karyawan_temp') ?>" class="menu-link">Karyawan Training dan Percobaan</a></td>
+                        </tr>
+                        <tr>
+                            
+                            <td><a href="<?= base_url('Master/Page_his/karyawan_out_temp') ?>" class="menu-link">Karyawan Training dan Percobaan Keluar</a></td>
+                        </tr>
+                        <tr>
+                            
+                            <td><a href="<?= base_url('Master/Page_his/divisi') ?>" class="menu-link">Daftar Divisi</a></td>
+                        </tr>
+                        <tr>
+                            
+                            <td><a href="<?= base_url('Master/Page_his/karyawan_mutasi') ?>" class="menu-link">Log Mutasi</a></td>
+                        </tr>
+                        <tr>
+                            
+                            <td><a href="<?= base_url('Master/Page_his/departemen') ?>" class="menu-link">Daftar Departemen</a></td>
+                        </tr>
+                        <tr>
+                            
+                            <td><a href="<?= base_url('Master/Page_his/section') ?>" class="menu-link">Daftar Section</a></td>
+                        </tr>
+                        <tr>
+                        
+                            <td><a href="<?= base_url('Master/Page_his/shift') ?>" class="menu-link">Daftar Shift</a></td>
+                        </tr>
+                        <tr>
+                            
+                            <td><a href="<?= base_url('Master/Page_his/jabatan') ?>" class="menu-link">Daftar Jabatan</a></td>
+                        </tr>
+                        <tr>
+                        
+                            <td><a href="<?= base_url('Master/Page_his/golongan') ?>" class="menu-link">Daftar Golongan</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+                </div>
+            </div>
+            <br>
+    
+</div>
+
+<style scoped>
+     .card {
+        border: none;
+    }
+
+    .card-body {
+        padding: 1.5rem;
+    }
+
+    .table {
+        margin-bottom: 0;
+        border: none;
+    }
+
+    .menu-link {
+        text-decoration: none;
+        color: #333;
+        display: block;
+        padding: 1rem;
+        border-radius: 8px;
+        transition: color 0.3s, background-color 0.3s, transform 0.3s, box-shadow 0.3s;
+    }
+
+    .menu-link:hover {
+        color: #fff;
+        background-color: #1cc88a; /* Ubah latar belakang menjadi hijau */
+        text-decoration: none;
+        transform: translateY(-5px);
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+    }
+
+    .back-link{
+        text-decoration: none;
+        color: #fff;
+        background-color: #1cc88a;
+        padding: 1rem;
+        border-radius: 8px;
+        transition: color 0.3s, background-color 0.3s, transform 0.3s, box-shadow 0.3s;
+    }
+
+    .back-link:hover{
+        color: #fff;
+        background-color: #1cc88a; /* Ubah latar belakang menjadi hijau */
+        text-decoration: none;
+        transform: translateY(-5px);
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
+    }
+</style>
