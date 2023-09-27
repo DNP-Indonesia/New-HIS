81e5c8a067a1c6bcf0c11130be8e5818badd3c38
FathanFalatansya <94509681+FathanFalatansya@users.noreply.github.com>
2023-09-27 11:07:11 +0700

    update_notif

--- application/views/layout/v_dashboard.php
+++ application/views/layout/v_dashboard.php
@@ -8,7 +8,9 @@
         </h1>
         <p class="lead text-gray-800 mb-5">HRCA Information System</p>
         <p class="text-gray-500 mb-0">Selamat datang, Salam Sehat!</p>
+        <?php if ($karyawanAkanPensiun != NULL) : ?>
         <div class="notifikasi" id="notifikasi"></div>
+        <?php endif; ?>
     </div>
 </div>
 <br>
